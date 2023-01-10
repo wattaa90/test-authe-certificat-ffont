5bf123379f0d3b1a89a4fc704de8dc187845f3d1
wattaa90 <wattaa90@users.noreply.github.com>
2023-01-10 11:55:58 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test-authe-certificat-ffont';
+
+
+  public result = "";
+
+
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+
+    this.httpClient.get('https://localhost:8080/connect'
+    , {responseType: 'text'}
+    ).subscribe(value => {
+      this.result = value;
+    })
+  }
+
+
 }
